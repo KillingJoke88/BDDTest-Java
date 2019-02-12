514ccbb503f7d9a05de21dc652127df09fa3ba2f
ylebreton <yoann.le-breton@gfi.fr>
2019-02-12 09:30:09 +0100

    no message

--- src/test/java/stepDefinitions/essai/StepDefs_DataDrivenSteps.java
+++ src/test/java/stepDefinitions/essai/StepDefs_DataDrivenSteps.java
@@ -1,14 +1,15 @@
 package stepDefinitions.essai;
 
 import cucumber.api.java.en.Then;
-import seleniumPages.Page_GoogleHomepage;
+//import seleniumPages.Page_GoogleHomepage;
 
 public class StepDefs_DataDrivenSteps {
 
-	Page_GoogleHomepage googleHomepage = new Page_GoogleHomepage();
+	//Page_GoogleHomepage googleHomepage = new Page_GoogleHomepage();
 	
 	@Then("I search for {string} on Google")
 	public void i_search_for_on_Google(String string) {
-		googleHomepage.searchOnGoogleHomepage(string);
+		System.out.println("Search on Google Page : " + string);
+		//googleHomepage.searchOnGoogleHomepage(string);
 	}
 }
--- src/test/java/stepDefinitions/essai/StepDefs_Essai.java
+++ src/test/java/stepDefinitions/essai/StepDefs_Essai.java
@@ -5,43 +5,43 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import seleniumPages.Page_GoogleHomepage;
+//import seleniumPages.Page_GoogleHomepage;
 
 public class StepDefs_Essai {
 
-	Page_GoogleHomepage googleHomepage = new Page_GoogleHomepage();
+	//Page_GoogleHomepage googleHomepage = new Page_GoogleHomepage();
 	
 	@Given("I launch Chrome browser")
 	public void i_launch_Chrome_browser() {
-		//System.out.println("Launch Chrome");
-		System.setProperty("webdriver.chrome.driver", "D:\\TOOLS\\chromedriver\\chromedriver.exe");
+		System.out.println("Launch Chrome");
+		/*System.setProperty("webdriver.chrome.driver", "D:\\TOOLS\\chromedriver\\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("disable-infobars");
 		
-		googleHomepage.launchBrowser();
+		googleHomepage.launchBrowser();*/
 	}
 
 	@When("I open Google Homepage")
 	public void i_open_Google_Homepage() {
-		//System.out.println("Open Google");
-		googleHomepage.openGoogleURL();
+		System.out.println("Open Google");
+		//googleHomepage.openGoogleURL();
 	}
 
 	@Then("I verify that the page displays search text box")
 	public void i_verify_that_the_page_displays_search_text_box() {
-		//System.out.println("Check search box");
-		googleHomepage.checkSearchBoxIsDisplayed();
+		System.out.println("Check search box");
+		//googleHomepage.checkSearchBoxIsDisplayed();
 	}
 
 	@Then("the page displays Google Search button")
 	public void the_page_displays_Google_Search_button() {
-		//System.out.println("Check search button");
-		googleHomepage.checkGoogleSearchButtonIsDisplayed();
+		System.out.println("Check search button");
+		//googleHomepage.checkGoogleSearchButtonIsDisplayed();
 	}
 
 	@Then("the page displays Im Feeling Lucky button")
 	public void the_page_displays_Im_Feeling_Lucky_button() {
-		//System.out.println("Check feeling lucky button");
-		googleHomepage.checkImFeelingLuckyButtonIsDisplayed();
+		System.out.println("Check feeling lucky button");
+		//googleHomepage.checkImFeelingLuckyButtonIsDisplayed();
 	}
 }
